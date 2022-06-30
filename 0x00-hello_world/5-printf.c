a0e530de2d5b85f92d6ba9e84307985842edb47b
Lawal Ayomide <layomide81@gmail.com>
2022-06-30 16:08:02 -0700

    printf/

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *main - to be called
- *return:0 for the int
- */
+* main - to be called
+* return:0 for the int
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
