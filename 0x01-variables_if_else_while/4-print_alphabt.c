c019303abed78eca4228c576562761fc0ee3bac2
Lawal Ayomide <layomide81@gmail.com>
2022-07-01 14:33:47 -0700

    ready

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,18 +6,13 @@
  *Return: Always (Success)
  */
 int main(void)
-{
+{ 
 	char c;
-
 	for (c = 'a'; c <= 'z'; c++)
-		{
+	{
 	if (c != 'e' && c != 'q')
-		{
-			putchar(c);
-					}
-					}
-
+	{ putchar(c); }
+			}
 	putchar('\n');
-
 	return (0);
 }
