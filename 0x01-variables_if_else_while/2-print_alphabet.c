6360e26e0e1fc9a628b8bfa5bc605dac2cec02f7
Lawal Ayomide <layomide81@gmail.com>
2022-07-01 14:27:09 -0700

    ready

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,8 +4,8 @@
 * Return: Always (Success)
 */
 int main(void)
-	{ char c; 
-	for (c = 'a'; c <= 'z'; c++) 
-	putchar(c); 	
+	{ char c;
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
 	putchar('\n');
 	return (0); }
