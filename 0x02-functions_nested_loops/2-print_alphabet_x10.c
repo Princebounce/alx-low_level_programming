1ea274caaf2a5887fa6577ede4b06dbf8c206349
Lawal Ayomide <layomide81@gmail.com>
2022-07-05 16:47:31 -0700

    ready

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,15 +10,19 @@ void print_alphabet_x10(void)
 {
 	char x;
 	int a = 0;
+
 	while (a < 10)
 	{
-	 for (x = 'a'; x <= 'z'; x++)
-	 {
-	 	_putchar(x);
 
-	 }
-	 _putchar('\n');
-	 a++
+	 for (x = 'a'; x <= 'z'; x++)
+
+	{
+		_putchar(x);
+
+	}
+	_putchar('\n');
+
+	a++;
 	}
 
 }
