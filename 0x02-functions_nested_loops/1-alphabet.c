c429aa7a8263cf4e94ed28b64731cc8615ca50bb
Lawal Ayomide <layomide81@gmail.com>
2022-07-05 15:52:56 -0700

    ready

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - process alphabets in lower case
+ *
+ * Return : 0 (Success)
+ *
+ */
+void print_alphabet(void)
+{
+	char x;
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar(x);
+}
