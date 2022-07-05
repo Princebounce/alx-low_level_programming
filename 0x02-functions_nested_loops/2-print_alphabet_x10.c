88677136ad4b975d2dcebf53d532f8d31364bcda
Lawal Ayomide <layomide81@gmail.com>
2022-07-05 16:15:05 -0700

    real

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets in lowercse 10times
+ *
+ * Return : 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char x;
+	int a = 0;
+	while (a < 10)
+	{
+	 for (x = 'a'; r <= 'z'; r++)
+	 {
+	 	_putchar(r);
+
+	 }
+	 _putchar('\n');
+	 a++
+	}
+
+}
