d1ef043de06d9b2ff7642a06dd1fb29df8696175
Lawal Ayomide <layomide81@gmail.com>
2022-07-05 19:10:30 -0700

    again

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ *
+ * @x: first integer
+ * @y: second integer
+ *
+ * Return: sum of x and y
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
