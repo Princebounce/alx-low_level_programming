afc7a0cf613604a4ca952ac02ffdf1ddccef13d5
Lawal Ayomide <layomide81@gmail.com>
2022-07-05 17:44:11 -0700

    again

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,19 +5,19 @@
  *
  * @c: integer 1 or 0
  *
- * Return : 1 lowercase 0 No
+ * Return: 1 lowercase 0 No
  *
  */
 
 int _islower(int c)
 {
 
-	if (c >= 97 && c <=122)
+	if (c >= 97 && c <= 122)
 
 	{
 	return (1);
 	}
-	
+
 	else
 	{
 		return (0);
