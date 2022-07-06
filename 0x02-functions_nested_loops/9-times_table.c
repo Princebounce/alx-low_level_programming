c030f0a2f1939dce06eaa70abde7f8487f6637a5
Lawal Ayomide <layomide81@gmail.com>
2022-07-05 18:59:43 -0700

    again

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * times_table - prints table frol 0 - 9
+ *
+ */
+
+void times_table(void)
+{
+int num, mul, prd;
+
+	for (num = 0; num <=9 num++)
+	{
+	_putchar ('0');
+
+	for (mul = 0; mul <= 9; mul++)
+	{
+		_putchar (',');
+		_putchar (' ');
+
+	prd = num * mul;
+
+	if (prd <= 9)
+		_putchar (' ');
+	else
+	{
+		_putchar ((prd / 10) + '0');
+		_putchar ((prd % 10) + '0');
+	}
+
+	}
+		_putchar ('\n');
+	}
+}
