08b05fa15145aa9b2773a14ab2b70c1a76db8a20
Lawal Ayomide <layomide81@gmail.com>
2022-07-07 15:54:26 -0700

    More_Nested_loop

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,17 @@
-#include "main.h"
+#include main.h
 
 /**
- * print_most_numbers - prints numbers 0-9 except 2,4
+ * print_numbers - prints numbers 0-9
  *
  * Return: void
  */
 
-void print_most_numbers(void)
+void print_numbers(void)
 {
 int c;
 
 for (c = '0'; c <= '9'; c++)
 {
-if (c != '2' && c != '4')
 _putchar(c);
 }
 _putchar('\n');
