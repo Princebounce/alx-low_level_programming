ee0ed78dc73473962c38d6531dffdf1dfd8c63ea
Lawal Ayomide <layomide81@gmail.com>
2022-07-20 05:34:34 -0700

    length of string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,12 +5,14 @@
  *
  * @s: the string to check the length of
  *
- * Return: an integer that displays the length of the string
+ * Return: lenghth of string
  */
 
 int _strlen_recursion(char *s)
 {
-if (*s != 0)
-return 0;
-return 1 + _strlen_recursion(s + 1);
+if (*s != '\0')
+return (1 + _strlen_recursion(s + 1));
+return (0);
 }
+
+
