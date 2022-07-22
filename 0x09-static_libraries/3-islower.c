30350cca18d778ff1074c992cfb3cde987d38b66
Lawal Ayomide <layomide81@gmail.com>
2022-07-22 08:25:29 -0700

    Understanding static library

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _islower - check lowercase
+ *
+ * @c: integer 1 or 0
+ *
+ * Return: 1 lowercase 0 No
+ *
+ */
+
+int _islower(int c)
+{
+
+	if (c >= 97 && c <= 122)
+
+	{
+	return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
