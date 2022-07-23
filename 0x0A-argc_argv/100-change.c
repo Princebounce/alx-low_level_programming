265f78baf8b3f41eadf1ae921492e8d5ba24b6e8
Lawal Ayomide <layomide81@gmail.com>
2022-07-22 17:32:57 -0700

    final commit

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,43 +11,43 @@
  */
 int main(int argc, char *argv[])
 {
-  int cents, coins = 0;
+int cents, coins = 0;
 
-  if (argc != 2)
-    {
-      printf("Error\n");
-      return (1);
-    }
-
-  cents = atoi(argv[1]);
-
-  while (cents > 0)
-    {
-      coins++;
-      if ((cents - 25) >= 0)
+if (argc != 2)
 	{
-	  cents -= 25;
-	  continue;
+	printf("Error\n");
+	return (1);
 	}
-      if ((cents - 10) >= 0)
-	{
-	  cents -= 10;
-	  continue;
-	}
-      if ((cents - 5) >= 0)
-	{
-	  cents -= 5;
-	  continue;
-	}
-      if ((cents - 2) >= 0)
-	{
-	  cents -= 2;
-	  continue;
-	}
-      cents--;
-    }
 
-  printf("%d\n", coins);
+	cents = atoi(argv[1]);
 
-  return (0);
+while (cents > 0)
+	{
+	coins++;
+	if ((cents - 25) >= 0)
+	{
+	cents -= 25;
+	continue;
+	}
+if ((cents - 10) >= 0)
+	{
+	cents -= 10;
+	continue;
+	}
+if ((cents - 5) >= 0)
+	{
+	cents -= 5;
+	continue;
+	}
+if ((cents - 2) >= 0)
+	{
+	cents -= 2;
+	continue;
+	}
+	cents--;
+	}
+
+	printf("%d\n", coins);
+
+	return (0);
 }
