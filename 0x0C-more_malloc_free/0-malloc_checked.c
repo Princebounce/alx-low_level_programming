4207ddaaddf1d67546982c719144223b5436f19e
Lawal Ayomide <layomide81@gmail.com>
2022-07-28 13:03:08 -0700

    test fix

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,20 +1,12 @@
-#include <stdlib.h>
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * malloc_checked - allocates memory using malloc
- *
- * @b: the number to allocate memory for
- *
- * Return: pointer to the allocated memory
- */
 
-void *malloc_checked(unsigned int b)
-{
-unsigned int *i;
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-	i = malloc(b);
-if (i == NULL)
-	exit(98);
-return (i);
-}
+#endif
