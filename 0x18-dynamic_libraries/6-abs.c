79f79f6cafea486b33cdf9717ae3a9e080c01e28
Taofik Lawal <layomide81@gmail.com.com>
2022-10-03 22:28:04 +0000

    Create the dynamic library libdynamic.so

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value
+ *
+ * @n: integer
+ *
+ * Return: return absolute value of int
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+
+		return (n);
+	}
+
+	else
+	{
+	return (-n);
+	}
+}
